bdc48f776e87d4d69c3a93a5f36eba36d9d12a17
Kenn-eth <royalmanbiz@gmail.com>
2024-06-20 09:47:20 +0100

    Integrated the frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import IPFSForm from "./IPFSForm.js";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Health Wallet</h1>
+      <IPFSForm />
+    </div>
+  );
+}
+
+export default App;
